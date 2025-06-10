a036744a92d654a48d92abb5afd14199a0f4d0c9
Matheus Augusto <auguzsto@gmail.com>
2025-06-10 19:57:38 -0300

    feat: Widget para apresentação de programas.

---
+++ lib/application/main/widgets/appicon_widget.dart
@@ -0,0 +1,23 @@
+import 'package:abas_desktop/application/main/main_viewmodel.dart';
+import 'package:abas_desktop/application/programa.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppIcon extends StatelessWidget {
+  final Programa programa;
+  const AppIcon({super.key, required this.programa});
+
+  @override
+  Widget build(BuildContext context) {
+    final mainViewModel = context.watch<MainViewModel>();
+    return Column(
+      children: [
+        IconButton.filled(
+          onPressed: () => mainViewModel.abrir(programa),
+          icon: Icon(Icons.edit_document, size: 18),
+        ),
+        Text(programa.titulo),
+      ],
+    );
+  }
+}
